42f3560bbc1a98c9ef7c0a4235ae5cd7f4c9e30c
Vincent Bouvier <vincent.bouvier1987@gmail.com>
2019-10-17 12:16:16 +0200

    Add description

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "lambda_version" {
-  value = aws_lambda_function.graphql.version
+  value       = aws_lambda_function.graphql.version
+  description = "the lambda version"
 }
 
 output "lambda_arn" {
-  value = aws_lambda_function.graphql.arn
+  value       = aws_lambda_function.graphql.arn
+  description = "the lambda arn"
 }
 
-output "lambda_name" {
-  value = aws_lambda_function.graphql.name
+output "lambda_function_name" {
+  value       = aws_lambda_function.graphql.function_name
+  description = "the lambda function name"
 }
