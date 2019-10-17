441a8e404db244ac2fccf09360a375737319cf0a
Vincent Bouvier <vincent.bouvier1987@gmail.com>
2019-10-17 12:15:18 +0200

    rename outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-output "graphql_lambda_version" {
+output "lambda_version" {
   value = aws_lambda_function.graphql.version
 }
 
-output "graphql_lambda_arn" {
+output "lambda_arn" {
   value = aws_lambda_function.graphql.arn
 }
 
-output "graphql_lambda_name" {
+output "lambda_name" {
   value = aws_lambda_function.graphql.name
 }
