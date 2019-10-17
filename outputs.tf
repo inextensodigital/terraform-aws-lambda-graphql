09f169138fc6b760cafbe040d5f189a287b7047e
Vincent Bouvier <vincent.bouvier1987@gmail.com>
2019-10-17 11:45:23 +0200

    add more outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "graphql_lambda_version" {
   value = aws_lambda_function.graphql.version
 }
+
+output "graphql_lambda_arn" {
+  value = aws_lambda_function.graphql.arn
+}
+
+output "graphql_lambda_name" {
+  value = aws_lambda_function.graphql.name
+}
