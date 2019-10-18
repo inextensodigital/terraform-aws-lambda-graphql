79d367ce589f97288d680c5c752de93401f3b5c6
Vincent Bouvier <vincent.bouvier1987@gmail.com>
2019-10-18 15:36:49 +0200

    export lambda role name

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "lambda_version" {
   description = "the lambda version"
 }
 
+output "lambda_role_name" {
+  value       = aws_iam_role.graphql_lambda_role.name
+  description = "the lambda arn"
+}
+
 output "lambda_arn" {
   value       = aws_lambda_function.graphql.arn
   description = "the lambda arn"
